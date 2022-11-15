41051c655ff6065b68a23ba6eb6dd069b8256166
Anastasiya <melihovaanastasya@yandex.ru>
2022-11-15 22:49:40 +0300

    20, 22

---
+++ 20/Program.cs
@@ -0,0 +1,8 @@
+﻿// 20. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+double x=Convert.ToDouble(Console.ReadLine());
+double y=Convert.ToDouble(Console.ReadLine());
+if (x>0 && y>0) System.Console.WriteLine("1 четверть");
+if (x>0 && y<0) System.Console.WriteLine("2 четверть");
+if (x<0 && y<0) System.Console.WriteLine("3 четверть");
+if (x<0 && y>0) System.Console.WriteLine("4 четверть");
---
+++ 22/Program.cs
@@ -0,0 +1,10 @@
+﻿// 22. Программа проверяет пятизначное число на палиндромом.
+
+int a=Convert.ToInt32(Console.ReadLine());
+int a1,a2,a4,a5;
+a1=a/10000;
+a2=(a%10000)/1000;
+a4=(a%100)/10;
+a5=a%10;
+if ((a1==a5) && (a2==a4)) System.Console.WriteLine("палиндром");
+else System.Console.WriteLine("no");
