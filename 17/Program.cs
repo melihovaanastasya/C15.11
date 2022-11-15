5977f172a3bc95784b8d06457779000a0fdf5172
Anastasiya <melihovaanastasya@yandex.ru>
2022-11-15 22:28:20 +0300

    17

---
+++ 17/Program.cs
@@ -0,0 +1,5 @@
+﻿// 17. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+int a=Convert.ToInt32(Console.ReadLine());
+if (a==6 | a==7) System.Console.WriteLine("выходной");
+else System.Console.WriteLine("мимо");
